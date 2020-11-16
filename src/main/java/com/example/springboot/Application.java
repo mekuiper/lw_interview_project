2b95f5d1eaa2f9b7d5858ca872fed052e889ff42
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2020-11-15 23:48:26 -0500

    all api features working todo cleanup

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -5,14 +5,21 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	   
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 	@Bean
--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,97 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
+	
+	@Value("${spring.solr.server}")
+	private String solrServer;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+	
+	@GetMapping("/testQuerySolr")
+	public ResponseEntity<String> querySolr(@RequestParam(value = ""
+			+ "", defaultValue = "peanutbutter") String searchTerm) {
+		return restTemplate.getForEntity(solrServer + "/select?q=id:"+searchTerm,  String.class);
+		
+	}
+	
+	@PostMapping(path="/createcustomers", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<String> createCustomer(@RequestBody String customers) {
+
+		System.out.println("/creatcustomers: " + customers);
+		//curl -X POST -H 'Content-Type: application/json' 'http://localhost:8983/solr/my_collection/update' --data-binary '
+		//curl -X POST -H 'Content-Type: application/json' 'http://localhost:8087/creatcustomers' --data-binary '
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> entity = new HttpEntity<String>(customers,headers);
+		
+		ResponseEntity<String> response = restTemplate.postForEntity(
+				solrServer + "/update?commit=true", entity , String.class);
+		
+		System.out.println("ResponseEntity<String> response " + response);
+		
+		return response;
+//		
+	}
+	
+	@GetMapping("/listcustomers")
+	public ResponseEntity<String> listCustomers(@RequestParam(value = "", defaultValue = "*:*") String searchTerm) {
+		return restTemplate.getForEntity(solrServer + "/select?q="+searchTerm,  String.class);	
+	}
+	
+	@GetMapping("/getcustomerbyid")
+	public ResponseEntity<String> getCustomerById(@RequestParam(value = "", defaultValue = "") String id) {
+		return restTemplate.getForEntity(solrServer + "/select?q=id:"+id,  String.class);	
+	}
+	
+//	@PostMapping(path="/deletecustomerbyid", consumes = "application/json", produces = "application/json")
+	@GetMapping(path="/deletecustomerbyid")
+	public ResponseEntity<String> deleteCustomerById(@RequestParam (value = "", defaultValue = "")String id) {
+
+		System.out.println("/deletecustomerbyid: " + id);
+		//curl -X POST -H 'Content-Type: application/json' 'http://localhost:8983/solr/my_collection/update' --data-binary '
+		//curl -X POST -H 'Content-Type: application/json' 'http://localhost:8087/creatcustomers' --data-binary '
+		
+		HttpHeaders headers = new HttpHeaders();
+//		curl http://localhost:8983/solr/mykeyspace.mysolr/update --data '<delete><query>color:red</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
+		headers.setContentType(MediaType.TEXT_XML);
+		//headers.setAcceptCharset(new );
+
+		HttpEntity<String> entity = new HttpEntity<String>("<delete><query>id:"+id+"</query></delete>",headers);
+		
+		ResponseEntity<String> response = restTemplate.postForEntity(
+				solrServer + "/update?commit=true", entity , String.class);
+		
+		System.out.println("ResponseEntity<String> response " + response);
+		
+		return response;
+//		
+	}
+
 
 }
